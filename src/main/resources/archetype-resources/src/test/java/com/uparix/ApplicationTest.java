2b7b4266ac05c11ed881084f82bf9027a7b9f20c
Andreas Neuenschwander <uparix@users.noreply.github.com>
2018-12-28 11:00:17 +0100

    added support for junit5

--- src/main/resources/archetype-resources/src/test/java/com/uparix/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/com/uparix/ApplicationTest.java
@@ -1,2 +1,20 @@
+package com.uparix;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@RunWith(JUnitPlatform.class)
+@SpringBootTest(classes = com.uparix.Application.class)
+@ExtendWith(SpringExtension.class)
 public class ApplicationTest {
+
+  @Test
+  public void contextLoads() {
+  }
+
 }

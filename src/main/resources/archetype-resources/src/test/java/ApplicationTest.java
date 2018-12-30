3a0e9653ec25a285885a348bbc3345fe6876b164
Andreas Neuenschwander <uparix@users.noreply.github.com>
2018-12-30 13:39:38 +0100

    use package from user

--- src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -1,12 +1,9 @@
-package com.uparix;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 
 @RunWith(JUnitPlatform.class)
 @SpringBootTest(classes = com.uparix.Application.class)
